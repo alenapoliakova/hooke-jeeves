7a02a9590317e56106ac75c2845e3cd81fc5243a
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-05-24 17:39:06 +0300

    Update test_hook_jeevs.py

--- tests/test_hook_jeevs.py
+++ tests/test_hook_jeevs.py
@@ -3,28 +3,30 @@ from src.algorithm import HookJeeves
 
 
 @pytest.mark.timeout(3)
-@pytest.mark.parametrize("function,initial_points,delta,epsilon,expected_points,expected_step", [
+@pytest.mark.parametrize("function,initial_points,delta,epsilon,explore_point,pattern_search_point,expected_points,expected_step", [
     # 1. Простая проверка с двумя слагаемыми в степени
-    ("x1**2 + x2**2", {"x1": -1, "x2": -1}, 0.5, 0.2, {"x1": 0, "x2": 0}, 0.125),
+    ("x1**2 + x2**2", {"x1": -1, "x2": -1}, 0.5, 0.2, {"x1": -0.5, "x2": -0.5}, {"x1": -1, "x2": -1}, {"x1": 0, "x2": 0}, 0.125),
     # 2. Проверка с выделением коэффициентов у переменных
-    ("(x1 - 2)**2 + 3*(x2**2)", {"x1": 0, "x2": -1},0.5, 0.2, {"x1": 2, "x2": 0}, 0.125),
+    ("(x1 - 2)**2 + 3*(x2**2)", {"x1": 0, "x2": -1},0.5, 0.2, {"x1": -0.5, "x2": -0.5}, {"x1": -1, "x2": -1}, {"x1": 2, "x2": 0}, 0.125),
     # 3. Тест из примера Кати
-    ("(x1+x2)**2 + (x2-1)**2", {"x1": 5, "x2": 6}, 2, 0.2, {"x1": -1, "x2": 1}, 0.125)
+    ("(x1+x2)**2 + (x2-1)**2", {"x1": 5, "x2": 6}, 2, 0.2, {"x1": -0.5, "x2": -0.5}, {"x1": -1, "x2": -1}, {"x1": -1, "x2": 1}, 0.125)
 ])
+
+#Тест на правильность выполнения
 def test_hook_jeeves(function, initial_points, delta, epsilon, expected_points, expected_step) -> None:
     algo = HookJeeves(function, initial_points, delta, epsilon)
     algo.solve()
     assert algo.current_point == expected_points
     assert algo.current_step == expected_step
 
-
-def test_explore() -> None:
-    algo = HookJeeves("x1**2 + x2**2", {"x1": -1, "x2": -1}, 0.5, 0.2)
+#Проверка полученных точек после исследовательного поиска
+def test_explore(function, initial_points, delta, epsilon, explore_point) -> None:
+    algo = HookJeeves(function, initial_points, delta, epsilon)
     algo.explore()
-    assert algo.current_point == {"x1": -0.5, "x2": -0.5}
+    assert algo.current_point == explore_point
 
-
-def test_pattern_search() -> None:
-    algo = HookJeeves("x1**2 + x2**2", {"x1": -1, "x2": -1}, 0.5, 0.2)
+#Проверка полученных точек после поиска по шаблону
+def test_pattern_search(function, initial_points, delta, epsilon, pattern_search_point) -> None:
+    algo = HookJeeves(function, initial_points, delta, epsilon)
     algo.pattern_search()
     assert algo.current_point == {"x1": -1, "x2": -1}
